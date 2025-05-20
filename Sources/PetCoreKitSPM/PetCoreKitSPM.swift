843722a6a22f4fa8a78292048a090c634ae9807a
DANIS <danis.preldzic@gmail.com>
2025-05-20 14:11:27 +0200

    Implement PetCoreKitSPM and PetCoreViewModel for enhanced data management
    - Created PetCoreKitSPM class as a shared container for dependency management.
    - Added petCoreDataSource property for accessing PetCoreDataSourceProtocol.
    - Introduced PetCoreViewModel class to manage user data and loading states.
    - Implemented asynchronous method getUser() for fetching user data with error handling.
    - Added alert management for success and failure responses in the view model.

--- Sources/PetCoreKitSPM/PetCoreKitSPM.swift
+++ Sources/PetCoreKitSPM/PetCoreKitSPM.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Factory
+import SwiftUI
+
+final public class PetCoreKitSPM: SharedContainer {
+    public let manager = ContainerManager()
+    public static let shared = PetCoreKitSPM()
+}
+
+public extension PetCoreKitSPM {
+    
+    var petCoreDataSource: Factory<PetCoreDataSourceProtocol> {
+        self { PetCoreDataSourceImplementation.shared }
+            .singleton
+    }
+    
+    @MainActor
+    static func start(onComplete: @escaping () -> Void) -> some View {
+        PetCoreCoordinator.shared.start(onComplete: onComplete)
+    }
+    
+}
---
+++ Sources/PetCoreKitSPM/ViewModels/PetCoreViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  PetCoreViewModel.swift
+//  PetCoreKitSPM
+//
+//  Created by Danis Preldzic on 20. 5. 2025..
+//
+import Foundation
+import Factory
+import SQAUtility
+import SwiftUICore
+import Shared_kit
+
+public class PetCoreViewModel: ObservableObject {
+    
+    @Injected(\PetCoreKitSPM.petCoreDataSource) var petCoreDataSource: PetCoreDataSourceProtocol
+    @Injected(\SQAUtility.storageManager) var storageManager: StorageManager
+    
+    public init() {}
+    
+    @Published public var isLoading: Bool = false
+    @Published var showAlert: Bool = false
+    @Published var alertMessage: String = ""
+    @Published var isSuccess: Bool = false
+    
+    @Published public var user: UserModel?
+    
+    public func getUser() async -> ResponseModel<String> {
+        isLoading = true
+        defer { isLoading = false }
+        
+        do {
+            let userID = storageManager.getUserID()
+            let dataResponse = try await petCoreDataSource.fetchUserById(userID: userID ?? "")
+            if let error = dataResponse.error {
+                return failureResponse(error.description)
+            } else {
+                user = dataResponse.data
+                return successResponse("Success")
+            }
+        } catch let error as NSError {
+            return failureResponse(error.description)
+        }
+    }
+    
+    
+    
+    
+    private func setAlert(message: String, success: Bool) {
+        self.alertMessage = message
+        self.isSuccess = success
+        self.showAlert = true
+    }
+    
+    private func failureResponse(_ message: String) -> ResponseModel<String> {
+        setAlert(message: message, success: false)
+        return ResponseModel<String>(data: nil, error: message)
+    }
+    
+    private func successResponse(_ message: String = "Success") -> ResponseModel<String> {
+        setAlert(message: message, success: true)
+        return ResponseModel<String>(data: message, error: nil)
+    }
+}
