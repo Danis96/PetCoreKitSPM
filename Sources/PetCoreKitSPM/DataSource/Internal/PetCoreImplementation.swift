1b5cadeb46d8ec2d3949cce47658d47f96ee072d
DANIS <danis.preldzic@gmail.com>
2025-05-20 13:52:20 +0200

    Implement PetCoreDataSource with CRUD operations for pets and users
    - Added PetCoreDataSourceImplementation class conforming to PetCoreDataSourceProtocol.
    - Implemented asynchronous methods for fetching, adding, editing, and deleting pets.
    - Integrated network service and storage manager for API calls and token management.
    - Enhanced error handling in response models for better error reporting.

--- Sources/PetCoreKitSPM/DataSource/Internal/PetCoreImplementation.swift
+++ Sources/PetCoreKitSPM/DataSource/Internal/PetCoreImplementation.swift
@@ -4,4 +4,126 @@
 //
 //  Created by Danis Preldzic on 20. 5. 2025..
 //
+import Shared_kit
+import Foundation
+import SQAUtility
+import SQAServices
+import Factory
 
+public final class PetCoreDataSourceImplementation: PetCoreDataSourceProtocol, @unchecked Sendable {
+    
+    public static let shared: PetCoreDataSourceProtocol = PetCoreDataSourceImplementation()
+    
+    public init() {}
+    
+    @Injected(\SQAServices.networkManager) var networkService: NetworkServiceManager
+    @Injected(\SQAServices.apiHeaderHelper) var headerHelper: ApiHeaders
+    @Injected(\SQAUtility.storageManager) var storageManager: StorageManager
+    
+    
+    
+    public func fetchUserById(userID: String) async throws -> ResponseModel<UserModel> {
+        do {
+           
+            let response = try await networkService.get(
+                path: PetCoreAPIPaths.shared.path(for: .getUserByID, concatValue: userID),
+                headers: headerHelper.getValue(type: .auth_app_json, accessToken: getAuthToken() ?? ""),
+                as: UserModel.self
+            )
+            
+            return ResponseModel(data: response, error: nil)
+        } catch {
+            return ResponseModel(data: nil, error: error.localizedDescription)
+        }
+    }
+    
+    public func addPet(pet: PetModel) async throws -> ResponseModel<PetModel> {
+        do {
+            
+            let jsonData = try JSONEncoder().encode(pet)
+            
+            let response = try await networkService.post(
+                path: PetCoreAPIPaths.shared.path(for: .createPet),
+                body: jsonData,
+                as: PetModel.self
+            )
+            
+            return ResponseModel(data: response, error: nil)
+            
+        } catch {
+            return ResponseModel(data: nil, error: error.localizedDescription)
+        }
+    }
+    
+    public func fetchOwnerPets(ownerID: String) async throws -> ResponseModel<[PetModel]> {
+        do {
+            
+            let response = try await networkService.get(
+                path: PetCoreAPIPaths.shared.path(for: .getOwnersPets, concatValue: ownerID),
+                headers: headerHelper.getValue(type: .auth_app_json, accessToken: getAuthToken() ?? ""),
+                as: [PetModel].self
+            )
+            
+            return ResponseModel(data: response, error: nil)
+        } catch {
+            return ResponseModel(data: nil, error: error.localizedDescription)
+        }
+    }
+    
+    public func fetchPetById(petID: String) async throws -> ResponseModel<PetModel> {
+        do {
+            
+            let response = try await networkService.get(
+                path: PetCoreAPIPaths.shared.path(for: .getPetByID, concatValue: petID),
+                headers: headerHelper.getValue(type: .auth_app_json, accessToken: getAuthToken() ?? ""),
+                as: PetModel.self
+            )
+            
+            return ResponseModel(data: response, error: nil)
+        } catch {
+            return ResponseModel(data: nil, error: error.localizedDescription)
+        }
+    }
+    
+    public func editPet(pet: PetModel) async throws -> ResponseModel<PetModel> {
+        do {
+            
+            let jsonData = try JSONEncoder().encode(pet)
+            
+            let response = try await networkService.put(
+                path: PetCoreAPIPaths.shared.path(for: .editPet, concatValue: pet.id ?? ""),
+                body: jsonData,
+                headers: headerHelper.getValue(type: .auth_app_json, accessToken: getAuthToken() ?? ""),
+                as: PetModel.self
+            )
+            
+            return ResponseModel(data: response, error: nil)
+        } catch {
+            return ResponseModel(data: nil, error: error.localizedDescription)
+        }
+    }
+    
+    public func deletePet(petID: String) async throws -> ResponseModel<String> {
+        do {
+            
+            _ = try await networkService.delete(
+                path: PetCoreAPIPaths.shared.path(for: .deletePet, concatValue: petID),
+                as: EmptyResponse.self
+            )
+            
+            return ResponseModel(data: "Success", error: nil)
+            
+        } catch {
+            return ResponseModel(data: nil, error: error.localizedDescription)
+        }
+    }
+    
+    private func getAuthToken() -> String? {
+        do {
+            let token = try storageManager.getSecureValue(forKey: .accessToken)
+            return token
+        } catch {
+            return nil
+        }
+    }
+}
